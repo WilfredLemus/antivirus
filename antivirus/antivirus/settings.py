db5e942bbabc6a028dc262022104315843694de0
WALR <wilfred.lean.15@gmail.com>
2015-05-24 22:24:51 -0600

    add template of app

--- antivirus/antivirus/settings.py
+++ antivirus/antivirus/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'antivirus.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'virus',
+        'USER': 'antivirus',
+        'PASSWORD': '123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
